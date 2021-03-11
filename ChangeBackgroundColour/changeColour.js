5cd6e1b0d41febc089f1d9dec4544d2904db3ead
Dimitar Penev <predator131@mail.bg>
2021-03-11 12:12:11 +0200

    Finish change background colour project

--- ChangeBackgroundColour/changeColour.js
+++ ChangeBackgroundColour/changeColour.js
@@ -1,10 +1,8 @@
 function foo() {
-   let body = document.querySelector('body');
-   let button = document.querySelector(''); 
-
-   button.addEventListener('click', (evt) => {
-
+   let buttons = document.querySelector('button'); 
+   let changeColourButton = buttons[0];
+   
+   changeColourButton.addEventListener('click', (evt) => {
+       document.body.style.backgroundColor = "#FFEF00";
    });
-
-   button.removeEventListener('click');
 }
