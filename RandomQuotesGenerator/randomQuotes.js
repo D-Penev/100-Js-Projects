7a33e85830d68800cc542ca0829b98a5dcc82635
Mitko <mitkopenev97@gmail.com>
2021-03-28 02:12:34 +0200

    Fix launch json file to work for Firefox, connecto to api to get quotes using fetch, add script src to index file

--- RandomQuotesGenerator/randomQuotes.js
+++ RandomQuotesGenerator/randomQuotes.js
@@ -1,6 +1,15 @@
 function randomQuotes() {
+   debugger;
    let quotesStorage = [];
-  
+   let quotesJson = 
+     fetch('https://type.fit/api/quotes')
+     .then(function(response) {
+         return response.json();
+     })
+
+   if (quotesJson !== null || quotesJson !== undefined) {
+       alert(quotesJson);
+   }
 }
 
 randomQuotes();
