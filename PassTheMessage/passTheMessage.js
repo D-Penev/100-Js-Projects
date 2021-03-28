1cfde0fc646741b5b579a57636dc1676887bb487
Mitko <mitkopenev97@gmail.com>
2021-03-28 13:37:42 +0300

    Finish pass the message project

--- PassTheMessage/passTheMessage.js
+++ PassTheMessage/passTheMessage.js
@@ -1,5 +1,14 @@
 function passTheMessage() {
-
+   let messageFromInput = document.getElementById('inputMessage');
+   let submitButton = document.getElementById('submitButton');
+   let messageContainer = document.getElementById('messageDiv');
+   if (messageFromInput !== null && submitButton !== null && messageContainer !== null) {
+       submitButton.addEventListener('click', (evt) => {
+          let messageText = messageFromInput.value;
+          messageContainer.append(`${messageText}`);
+          messageContainer.append('\n');
+      });
+   }
 }
 
 passTheMessage();
