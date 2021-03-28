825e8bb5fc9dce0586122f9cd713c589f1dd14ca
Mitko <mitkopenev97@gmail.com>
2021-03-28 02:21:20 +0200

    Refactor connection to api, implement logic for random quote selection

--- RandomQuotesGenerator/randomQuotes.js
+++ RandomQuotesGenerator/randomQuotes.js
@@ -1,15 +1,23 @@
 function randomQuotes() {
-   debugger;
    let quotesStorage = [];
    let quotesJson = 
      fetch('https://type.fit/api/quotes')
      .then(function(response) {
          return response.json();
-     })
+     }).then(function (data) {
+         debugger;
+        let randomIndex = Math.floor(Math.random() * data.length);
+        let currQuoteObj = data[randomIndex];
+        let parsedJson = JSON.parse(currQuoteObj);
+        
+     });
+   let quoteBody = document.getElementById('currQuote');
+   let quoteAuthor = document.getElementById('currQuoteAuthor');
 
-   if (quotesJson !== null || quotesJson !== undefined) {
-       alert(quotesJson);
+   if (quoteBody !== null & quoteAuthor !== null) {
+     
    }
+   
 }
 
 randomQuotes();
